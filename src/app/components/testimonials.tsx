715c1ab760bf1beda748fb3174ed48314edee8c0
Shahid Rafique <shahid.rafique@arbisoft.com>
2025-05-29 17:43:30 +0500

    page phase 1

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -2,23 +2,30 @@ import Image from "next/image";
 
 export default function Testimonials() {
   return (
-    <section className="section testimonials">
-      <h2 className="section-title">Hear it from our clients</h2>
-
-      <div className="testimonial">
-        <div className="testimonial-inner">
-          <Image
-            src="/images/testimonials/testimonial-image-1.png"
-            width={478}
-            height={439}
-            alt="Mcdonalds testimonial image"
-          />
-          <div className="testimonial-content">
-            <p className="testimonial-text">
-              With Sentimeter our customer engagement jumped by 10 folds in the first quarter alone. And helped reduce our complaint processing time from days to seconds.
-            </p>
-            <div className="testimonial-author">-Aamir Malik</div>
-            <div className="testimonial-role">Director</div>
+    <section className="section has-bg testimonials">
+      <div className="container">
+        <h2 className="section-title">Hear it from our clients</h2>
+      </div>
+      <div className="card">
+        <div className="container">
+          <div className="testimonial">
+            <div className="testimonial-inner">
+              <Image
+                src="/images/testimonials/testimonial-image-1.png"
+                width={478}
+                height={439}
+                alt="Mcdonalds testimonial image"
+              />
+              <div className="testimonial-content">
+                <p className="testimonial-text">
+                  With Sentimeter our customer engagement jumped by 10 folds in
+                  the first quarter alone. And helped reduce our complaint
+                  processing time from days to seconds.
+                </p>
+                <div className="testimonial-author">-Aamir Malik</div>
+                <div className="testimonial-role">Director</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/app/cx/phase1/page.tsx
@@ -0,0 +1,458 @@
+import Image from "next/image";
+import Clients from "../components/clients";
+import Link from "next/link";
+export default function page() {
+  return (
+    <article className="phase1">
+      {/* Sub Header */}
+      <section className="sub-header">
+        <div className="container">
+          <div className="sub-header-left">
+            <Image
+              src="/images/senti-ex.svg"
+              width={240}
+              height={96}
+              alt="senti ex"
+            />
+          </div>
+          <div className="sub-header-right">Back to SentiEX</div>
+        </div>
+      </section>
+
+      {/* Banner */}
+      <section className="banner">
+        <div className="container">
+          <div className="col">
+            <div className="content">
+              <h3>Make Feedback Fearless and Effortless</h3>
+              <h2 className="title">
+                Create a Safe & Seamless <span>Feedback</span> Culture
+              </h2>
+              <p>
+                Sentimeter empowers organizations to build a culture where
+                feedback is effortless, fearless, and actionable. By ensuring
+                every voice is heard—securely and inclusively—you can uncover
+                the insights needed to drive real employee satisfaction and
+                retention.
+              </p>
+              <Link href="#" className="button button--outline">
+                Request a Demo
+              </Link>
+            </div>
+          </div>
+          <div className="col">
+            <Image
+              src="/images/image-phase1-header.png"
+              width={515}
+              height={515}
+              alt="senti ex"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Comprehensive Feedback */}
+
+      <section className="section has-bg red-light comprehensive-feedback">
+        <div className="card">
+          <div className="container">
+            <div className="comprehensive-feedback-left">
+              <h2>
+                Comprehensive <span>Feedback</span> at Every Corner
+              </h2>
+              <div className="content">
+                <p>
+                  Sentimeter collects feedback across multiple channels, giving
+                  you a complete view of the employee experience. Whether
+                  through pulse surveys or unsolicited comments, our tools
+                  ensure no signal is missed.
+                </p>
+
+                <p>
+                  <i>
+                    <span>Why It Matters:</span> "When feedback is easy to give,
+                    employees are eager to share it."
+                  </i>
+                </p>
+              </div>
+              <Link href="#" className="button">
+                Explore Why Sentimeter
+              </Link>
+            </div>
+            <div className="comprehensive-feedback-right">
+              <h3>Key Features:</h3>
+              <div className="benefit">
+                <Image
+                  src={"/images/icon-comprehensive-1.svg"}
+                  width={60}
+                  height={60}
+                  alt="benefit"
+                />
+                <p>
+                  <span>Global Accessibility:</span> Multi-lingual, multi-modal
+                  feedback tools cater to diverse teams across geographies.
+                </p>
+              </div>
+              <div className="benefit">
+                <Image
+                  src={"/images/icon-comprehensive-2.svg"}
+                  width={60}
+                  height={60}
+                  alt="benefit"
+                />
+                <p>
+                  <span>Omnichannel Integration:</span> Collect insights from
+                  surveys, chat logs, emails, in-app tools, and more.
+                </p>
+              </div>
+              <div className="benefit">
+                <Image
+                  src={"/images/icon-comprehensive-3.svg"}
+                  width={60}
+                  height={60}
+                  alt="benefit"
+                />
+                <p>
+                  <span>Rich Media Support:</span> Gather deeper insights with
+                  video, image, and voice-based feedback.
+                </p>
+              </div>
+              <div className="benefit">
+                <Image
+                  src={"/images/icon-comprehensive-3.svg"}
+                  width={60}
+                  height={60}
+                  alt="benefit"
+                />
+                <p>
+                  <span>Push and Pull Options:</span> Combine proactive surveys
+                  with always-available feedback channels for a seamless
+                  experience.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Build Trust */}
+      <section className="section has-bg build-trust">
+        <div className="card">
+          <div className="container">
+            <div className="col">
+              <Image
+                src={"/images/image-build-trust-icon.png"}
+                width={125}
+                height={125}
+                alt="benefit"
+              />
+            </div>
+
+            <div className="col content">
+              <h3>
+                Build <span>Trust</span> Through Anonymity
+              </h3>
+              <p>
+                Honest feedback is fearless feedback. Sentimeter ensures
+                confidentiality, allowing employees to share their thoughts
+                openly without fear of repercussions.
+              </p>
+              <h5>Key Features:</h5>
+              <div className="row">
+                <div className="col">
+                  <div className="icon">
+                    <Image
+                      src={"/images/icon-build-trust1.svg"}
+                      width={86}
+                      height={86}
+                      alt="benefit"
+                    />
+                  </div>
+                  <h4>Guaranteed Anonymity</h4>
+                  <p>
+                    Secure systems ensure privacy for fearless participation.
+                  </p>
+                </div>
+
+                <div className="col">
+                  <div className="icon">
+                    <Image
+                      src={"/images/icon-build-trust2.svg"}
+                      width={86}
+                      height={86}
+                      alt="benefit"
+                    />
+                  </div>
+                  <h4>Transparency Tools</h4>
+                  <p>
+                    Foster trust by demonstrating how feedback drives real
+                    improvements.
+                  </p>
+                </div>
+
+                <div className="col">
+                  <div className="icon">
+                    <Image
+                      src={"/images/icon-build-trust3.svg"}
+                      width={86}
+                      height={86}
+                      alt="benefit"
+                    />
+                  </div>
+                  <h4>Trust-Building Analytics</h4>
+                  <p>
+                    Build stronger relationships between employees and
+                    leadership.
+                  </p>
+                </div>
+              </div>
+              <p>
+                <span>Why It Matters: </span>
+                <i>
+                  "When feedback is easy to give, employees are eager to share
+                  it."
+                </i>
+              </p>
+              <div className="button button--outline">
+                Create a Safe Feedback Space
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Automation */}
+      <section className="section automation">
+        <div className="container">
+          <h3>Automation That Works for You</h3>
+          <p>
+            Save time and improve participation with automated surveys and
+            response tracking. Sentimeter ensures you ask the right questions at
+            the right time.
+          </p>
+        </div>
+        <div className="container">
+          <div className="col">
+            <h5>Key Features:</h5>
+            <ul>
+              <li>
+                <p>
+                  <span>Timely Pulse Surveys:</span> Automate feedback
+                  collection after key moments like onboarding or performance
+                  reviews.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <span>Smart Sampling:</span> Avoid survey fatigue with
+                  optimized distribution strategies.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <span>Real-Time Feedback:</span> Act on insights as they come
+                  in, with minimal effort.
+                </p>
+              </li>
+            </ul>
+            <p className="note">
+              <span>Why It Matters:</span> "Automation eliminates friction and
+              ensures you capture meaningful feedback every time"
+            </p>
+            <Link href="#" className="button">
+              Experience Smarter Employee Surveys
+            </Link>
+          </div>
+          <div className="col">
+            <Image
+              src={"/images/image-automation.png"}
+              width={708}
+              height={444}
+              alt="Automation"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Proactive */}
+      <section className="section has-bg blue proactive">
+        <div className="card">
+          <div className="container">
+            <div className="col">
+              <div className="content">
+                <h2>Proactive Listening Across the Lifecycle</h2>
+                <p>
+                  Capture feedback throughout the employee journey with tools
+                  tailored to each stage—from recruitment to exit.
+                </p>
+                <p className="note">
+                  <span>Why It Matters:</span> "Understanding employees
+                  holistically ensures every stage of their journey is
+                  impactful."
+                </p>
+                <Link href={"#"} className="button">
+                  See Lifecycle Insights in Action
+                </Link>
+              </div>
+            </div>
+            <div className="col">
+              <h5 className="key-features">Key Features</h5>
+              <div className="container">
+                <div className="col">
+                  <Image
+                    src={"/images/ternado.svg"}
+                    width={303}
+                    height={341}
+                    alt="ternado"
+                  />
+                </div>
+                <div className="col features">
+                  <div className="feature">
+                    <div className="icon">
+                      <Image
+                        src={"/images/icon-proactive-1.svg"}
+                        width={60}
+                        height={60}
+                        alt="ternado"
+                      />
+                    </div>
+                    <div className="content">
+                      <h5>Lifecycle Feedback Mapping</h5>
+                      <p>
+                        Monitor experiences across all touchpoints, including
+                        onboarding, training, and performance reviews.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="feature">
+                    <div className="icon">
+                      <Image
+                        src={"/images/icon-proactive-2.svg"}
+                        width={60}
+                        height={60}
+                        alt="ternado"
+                      />
+                    </div>
+                    <div className="content">
+                      <h5>End-to-End Analytics</h5>
+                      <p>
+                        Track trends and patterns to see the bigger picture.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="feature">
+                    <div className="icon">
+                      <Image
+                        src={"/images/icon-proactive-3.svg"}
+                        width={60}
+                        height={60}
+                        alt="ternado"
+                      />
+                    </div>
+                    <div className="content">
+                      <h5>Responsive Listening</h5>
+                      <p>Stay open to feedback when employees need it most.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Why Sentimeter for Feedback? */}
+
+      <section className="section why">
+        <div className="container">
+          <h3>
+            Why <span>Sentimeter</span> for Feedback?
+          </h3>
+          <div className="row">
+            <div className="col">
+              <div className="feature feature-1">
+                <div className="content">
+                  <h5 className="red-70">Data Security</h5>
+                  <p>
+                    Guarantee privacy and security for fearless participation.
+                  </p>
+                </div>
+                <div className="icon">
+                  <Image
+                    src={"/images/icon-why-1.svg"}
+                    width={60}
+                    height={60}
+                    alt="ternado"
+                  />
+                </div>
+              </div>
+              <div className="feature  feature-2">
+                <div className="content">
+                  <h5 className="red-80">Real-Time Insights</h5>
+                  <p>
+                    Act quickly with live dashboards and detailed reporting.
+                  </p>
+                </div>
+                <div className="icon">
+                  <Image
+                    src={"/images/icon-why-2.svg"}
+                    width={60}
+                    height={60}
+                    alt="ternado"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <Image
+                src={"/images/image-why.svg"}
+                width={366}
+                height={366}
+                alt="ternado"
+              />
+            </div>
+            <div className="col">
+              <div className="feature feature-3">
+                <div className="icon">
+                  <Image
+                    src={"/images/icon-why-3.svg"}
+                    width={60}
+                    height={60}
+                    alt="ternado"
+                  />
+                </div>
+                <div className="content">
+                  <h5>Employee-Centric Design</h5>
+                  <p>
+                    Build trust and engagement with tools designed for
+                    inclusivity.
+                  </p>
+                </div>
+              </div>
+              <div className="feature feature-4">
+                <div className="icon">
+                  <Image
+                    src={"/images/icon-why-4.svg"}
+                    width={60}
+                    height={60}
+                    alt="ternado"
+                  />
+                </div>
+                <div className="content">
+                  <h5 className="red-90">Scalable Solutions</h5>
+                  <p>
+                    Adapt seamlessly from small teams to global enterprises.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Link href={"#"} className="button">
+            Empower Employees Through Feedback
+          </Link>
+        </div>
+      </section>
+    </article>
+  );
+}
