b69b2e239bbec5de07a282342cf352c603c45772
Shahid Rafique <shahid.rafique@arbisoft.com>
2025-06-13 18:28:33 +0500

    added the sub nav

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -30,6 +30,219 @@ export default function Header() {
                   </li>
                   <li className="nav__item">
                     <Link href="/cx">Product</Link>
+                    <div className="sub-nav">
+                      <div className="container">
+                        <div className="left-tabs">
+                          <a href="#" className="active">
+                            Customer Experience
+                          </a>
+                          <a href="#">Employee Experience</a>
+                        </div>
+                        <div className="right-nav">
+                          <div className="logo">
+                            <Image
+                              src={"/images/senti-ex.svg"}
+                              width={237}
+                              height={88}
+                              alt="senti ex"
+                            />
+                          </div>
+                          <nav className="nav-links">
+                            <div className="sentiex">
+                              <ul>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Explore SentiCX
+                                    </div>
+                                    <div className="link-sub-title">
+                                      CX at the core of Retention & growth
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Map Customer Journey with precision
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Build a strong foundation
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Capture Insights Everywhere
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Collect Feedback that matters
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Actionable Insights
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Turn Data into Actionable Insights
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Take Meaningful Action to Improve CX
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Improve Experience wih smarter ctions
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Report and Iterte
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Drive continuous improvement
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Net Promoter Score (NPs)
+                                    </div>
+                                    <div className="link-sub-title">
+                                      CX at the core of Retention & growth
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Recovery Management
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Empower your recover strategy
+                                    </div>
+                                  </a>
+                                </li>{" "}
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Text Analysis
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Smarter Text Analysis for Smarter Business
+                                      Decisions
+                                    </div>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                            {/* Senti CX */}
+                            <div className="senticx">
+                              <ul>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Explore SentiCX
+                                    </div>
+                                    <div className="link-sub-title">
+                                      CX at the core of Retention & growth
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Map Customer Journey with precision
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Build a strong foundation
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Capture Insights Everywhere
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Collect Feedback that matters
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Actionable Insights
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Turn Data into Actionable Insights
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Take Meaningful Action to Improve CX
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Improve Experience wih smarter ctions
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Report and Iterte
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Drive continuous improvement
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Net Promoter Score (NPs)
+                                    </div>
+                                    <div className="link-sub-title">
+                                      CX at the core of Retention & growth
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Recovery Management
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Empower your recover strategy
+                                    </div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#">
+                                    <div className="link-title">
+                                      Text Analysis
+                                    </div>
+                                    <div className="link-sub-title">
+                                      Smarter Text Analysis for Smarter Business
+                                      Decisions
+                                    </div>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                          </nav>
+                        </div>
+                      </div>
+                    </div>
                   </li>
                   <li className="nav__item">
                     <Link href="/solution">Pricing</Link>
--- src/app/components/home/hero.tsx
+++ src/app/components/home/hero.tsx
@@ -71,35 +71,50 @@ export default function Hero() {
 
       <div className="home-nav">
         <nav className="nav nav--home">
-          <ul>
-            <li className="nav__item nav__item--home">
-              <Link href="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                </svg>
-              </Link>
-            </li>
-            <li className="nav__item">
-              <Link href="/solution">AI Text Analytics</Link>
-            </li>
-            <li className="nav__item">
-              <Link href="/cx">Product</Link>
-            </li>
-            <li className="nav__item">
-              <Link href="/solution">Pricing</Link>
-            </li>
-          </ul>
+          <div className="wrapper">
+            <ul>
+              <li className="nav__item nav__item--home">
+                <Link href="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                  </svg>
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link href="/solution">AI Text Analytics</Link>
+              </li>
+              <li className="nav__item">
+                <Link href="/cx">Product</Link>
+                <div className="home-sub-nav">
+                  <ul>
+                    <li>
+                      <Link href="/cx">Customer Experience </Link>
+                    </li>
+                    <li>
+                      <Link href="/cx">Employee Experience </Link>
+                    </li>
+                    <li>
+                      <Link href="/cx">CX Strategy & Execution</Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="nav__item">
+                <Link href="/solution">Pricing</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </section>
